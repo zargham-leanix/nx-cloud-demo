181a756f461b3fe607ed55f61ea4a0832eba411a
Zargham Khan <zargham.khan@leanix.net>
2025-01-20 21:46:55 +0100

    Generator creates libs

--- tools/local-plugin/src/generator/libs.ts
+++ tools/local-plugin/src/generator/libs.ts
@@ -1,22 +1,79 @@
 import {
-  addProjectConfiguration,
-  formatFiles,
-  generateFiles,
-  Tree,
-} from '@nx/devkit';
-import * as path from 'path';
-import { LibsGeneratorSchema } from './schema';
+  componentGenerator,
+  libraryGenerator,
+  UnitTestRunner,
+} from '@nx/angular/generators';
+import { formatFiles, Tree } from '@nx/devkit';
 
-export async function libsGenerator(tree: Tree, options: LibsGeneratorSchema) {
-  const projectRoot = `libs/${options.name}`;
-  addProjectConfiguration(tree, options.name, {
-    root: projectRoot,
-    projectType: 'library',
-    sourceRoot: `${projectRoot}/src`,
-    targets: {},
-  });
-  generateFiles(tree, path.join(__dirname, 'files'), projectRoot, options);
+export async function libsGenerator(tree: Tree) {
+  const libs = [...genNum(1, 100)];
+
+  // for (const lib of libs) {
+  //   await generateLib(tree, lib);
+  // }
+
+  tree.write(
+    'apps/nx-cloud-demo/src/app/app.component.ts',
+    createAppComponent(libs)
+  );
   await formatFiles(tree);
 }
 
 export default libsGenerator;
+
+async function generateLib(tree: Tree, index: number) {
+  const name = `lib${index}`;
+  await libraryGenerator(tree, {
+    name: name,
+    directory: `libs/${name}`,
+    unitTestRunner: UnitTestRunner.Jest,
+    buildable: true,
+    standaloneConfig: true,
+    standalone: false,
+    prefix: 'nx',
+    publishable: true,
+    importPath: `@demo/${name}`,
+    style: 'scss',
+  });
+
+  for (let i = 0; i < 50; i++) {
+    const cmpName = `${name}-demo${i}`;
+    console.log(cmpName);
+    await componentGenerator(tree, {
+      path: `libs/${name}/src/lib/components/${cmpName}/${cmpName}`,
+      name: cmpName,
+      standalone: false,
+      changeDetection: 'OnPush',
+      export: true,
+    });
+  }
+}
+
+function createAppComponent(indexes: number[]) {
+  return `
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NxWelcomeComponent } from './nx-welcome.component';
+${indexes
+  .map((it) => `import {Lib${it}Module } from '@demo/lib${it}'`)
+  .join(';')}
+
+@Component({
+  standalone: true,
+  imports: [
+  NxWelcomeComponent,
+  RouterModule,
+${indexes.map((it) => `Lib${it}Module`)}
+],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'nx-cloud-demo';
+}
+`;
+}
+function* genNum(from: number, to: number) {
+  for (let i = from; i <= to; i++) yield i;
+}
