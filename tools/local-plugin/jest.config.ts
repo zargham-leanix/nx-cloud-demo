c41145e4aaa88210f0f9cf418bee8e1b87b25cce
Zargham Khan <zargham.khan@leanix.net>
2025-01-20 17:55:11 +0100

    Create empty generator

---
+++ tools/local-plugin/jest.config.ts
@@ -0,0 +1,9 @@
+export default {
+  displayName: 'local-plugin',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/tools/local-plugin',
+};
