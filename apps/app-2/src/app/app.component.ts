bcace2ca2cc4093de549de0a210d4ff8d2198208
Zargham Khan <zargham.khan@leanix.net>
2025-01-25 23:38:02 +0100

    add second app

---
+++ apps/app-2/src/app/app.component.ts
@@ -0,0 +1,117 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Lib1Module } from '@demo/lib1';
+import { Lib10Module } from '@demo/lib10';
+import { Lib11Module } from '@demo/lib11';
+import { Lib12Module } from '@demo/lib12';
+import { Lib13Module } from '@demo/lib13';
+import { Lib14Module } from '@demo/lib14';
+import { Lib15Module } from '@demo/lib15';
+import { Lib16Module } from '@demo/lib16';
+import { Lib17Module } from '@demo/lib17';
+import { Lib18Module } from '@demo/lib18';
+import { Lib19Module } from '@demo/lib19';
+import { Lib2Module } from '@demo/lib2';
+import { Lib20Module } from '@demo/lib20';
+import { Lib21Module } from '@demo/lib21';
+import { Lib22Module } from '@demo/lib22';
+import { Lib23Module } from '@demo/lib23';
+import { Lib24Module } from '@demo/lib24';
+import { Lib25Module } from '@demo/lib25';
+import { Lib26Module } from '@demo/lib26';
+import { Lib27Module } from '@demo/lib27';
+import { Lib28Module } from '@demo/lib28';
+import { Lib29Module } from '@demo/lib29';
+import { Lib3Module } from '@demo/lib3';
+import { Lib30Module } from '@demo/lib30';
+import { Lib31Module } from '@demo/lib31';
+import { Lib32Module } from '@demo/lib32';
+import { Lib33Module } from '@demo/lib33';
+import { Lib34Module } from '@demo/lib34';
+import { Lib35Module } from '@demo/lib35';
+import { Lib36Module } from '@demo/lib36';
+import { Lib37Module } from '@demo/lib37';
+import { Lib38Module } from '@demo/lib38';
+import { Lib39Module } from '@demo/lib39';
+import { Lib4Module } from '@demo/lib4';
+import { Lib40Module } from '@demo/lib40';
+import { Lib41Module } from '@demo/lib41';
+import { Lib42Module } from '@demo/lib42';
+import { Lib43Module } from '@demo/lib43';
+import { Lib44Module } from '@demo/lib44';
+import { Lib45Module } from '@demo/lib45';
+import { Lib46Module } from '@demo/lib46';
+import { Lib47Module } from '@demo/lib47';
+import { Lib48Module } from '@demo/lib48';
+import { Lib49Module } from '@demo/lib49';
+import { Lib5Module } from '@demo/lib5';
+import { Lib50Module } from '@demo/lib50';
+import { Lib6Module } from '@demo/lib6';
+import { Lib7Module } from '@demo/lib7';
+import { Lib8Module } from '@demo/lib8';
+import { Lib9Module } from '@demo/lib9';
+import { NxWelcomeComponent } from './nx-welcome.component';
+
+@Component({
+  standalone: true,
+  imports: [
+    NxWelcomeComponent,
+    RouterModule,
+    Lib1Module,
+    Lib2Module,
+    Lib3Module,
+    Lib4Module,
+    Lib5Module,
+    Lib6Module,
+    Lib7Module,
+    Lib8Module,
+    Lib9Module,
+    Lib10Module,
+    Lib11Module,
+    Lib12Module,
+    Lib13Module,
+    Lib14Module,
+    Lib15Module,
+    Lib16Module,
+    Lib17Module,
+    Lib18Module,
+    Lib19Module,
+    Lib20Module,
+    Lib21Module,
+    Lib22Module,
+    Lib23Module,
+    Lib24Module,
+    Lib25Module,
+    Lib26Module,
+    Lib27Module,
+    Lib28Module,
+    Lib29Module,
+    Lib30Module,
+    Lib31Module,
+    Lib32Module,
+    Lib33Module,
+    Lib34Module,
+    Lib35Module,
+    Lib36Module,
+    Lib37Module,
+    Lib38Module,
+    Lib39Module,
+    Lib40Module,
+    Lib41Module,
+    Lib42Module,
+    Lib43Module,
+    Lib44Module,
+    Lib45Module,
+    Lib46Module,
+    Lib47Module,
+    Lib48Module,
+    Lib49Module,
+    Lib50Module,
+  ],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'app-2';
+}
